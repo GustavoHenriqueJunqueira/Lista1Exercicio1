ae90427bc0c570219481f687c5c2e3cf3d751e43
Gustavo Henrique f. Junqueira <gustavohenriquejunqueira@hotmail.com>
2018-09-03 23:38:01 -0300

    Melhorias no programa antigo
    Em minha interpretação estava falando variáveis para se fazer o calculo corretamente.

--- Lista1Exercicio1.java
+++ Lista1Exercicio1.java
@@ -6,17 +6,26 @@ public class Lista1Exercicio1 {
 
     public static void main(String[] args) {
        
-        float ladoA, ladoB, area;
+        float ladoA, ladoB,ladoC, ladoD, area;
         
         System.out.println("Algoritimo para calcular a area de uma sala");
         
-        System.out.println("Digite a medida do lado da sala");
-        Scanner teclado = new Scanner(System.in);
-        ladoA = teclado.nextFloat();
-        System.out.println("Digite a medida do comprimento da sala");
-        ladoB = teclado.nextFloat();
+            System.out.println("Digite a medida do ladoA da sala");
+            Scanner teclado = new Scanner(System.in);
+            ladoA = teclado.nextFloat();
+            System.out.println("Digite a medida do ladoB da sala");
+            Scanner teclado = new Scanner(System.in);
+            ladoB = teclado.nextFloat();
+            System.out.println("Digite a medida do ladoC da sala");
+            Scanner teclado = new Scanner(System.in);
+            ladoC = teclado.nextFloat();
+            System.out.println("Digite a medida do ladoD da sala");
+            Scanner teclado = new Scanner(System.in);
+            ladoD = teclado.nextFloat();
+            System.out.println("Digite a medida da altura da sala");
+            AlturaA = teclado.nextFloat();
         
-        area=ladoA*ladoB;
+        area=(ladoA+ladoB+ladoC+ladoD)*AlturaA;
         
         System.out.println("A area todal desta sala é de: " + area);
         
